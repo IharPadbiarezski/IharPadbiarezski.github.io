f7ee8dad61e0918d182e23be3f794b602e58ed1d
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-12 23:17:10 +0300

    refactor: change structure of the project

--- js/bar.js
+++ js/bar.js
@@ -1,17 +1,17 @@
-var skillsDiv = jQuery('#skills');
+var skillsDiv = jQuery("#skills");
 
-$(window).on('scroll', function() {
+$(window).on("scroll", function() {
   var winT = jQuery(window).scrollTop(),
     winH = jQuery(window).height(),
     skillsT = skillsDiv.offset().top;
   if (winT + winH > skillsT) {
-    $('.html').animate({width:'90%'},2000);
-    $('.css').animate({width:'90%'},2000);
-    $('.js').animate({width:'40%'},2000);
-    $('.jQuery').animate({width:'50%'},2000);
-    $('.sass').animate({width:'60%'},2000);
-    $('.boot').animate({width:'65%'},2000);
-    $('.git').animate({width:'75%'},2000);
-    $('.ps').animate({width:'75%'},2000);
+    $(".html").animate({ width: "90%" }, 2000);
+    $(".css").animate({ width: "90%" }, 2000);
+    $(".js").animate({ width: "80%" }, 2000);
+    $(".jQuery").animate({ width: "60%" }, 2000);
+    $(".sass").animate({ width: "70%" }, 2000);
+    $(".boot").animate({ width: "70%" }, 2000);
+    $(".git").animate({ width: "75%" }, 2000);
+    $(".ps").animate({ width: "75%" }, 2000);
   }
 });
