19d602f061c2fb8d58c02eee70d18b992bc0d7c2
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-19 22:52:20 +0300

    feat: create react skill

--- js/bar.js
+++ js/bar.js
@@ -1,17 +1,17 @@
-var skillsDiv = jQuery("#skills");
+var skillsDiv = jQuery('#skills');
 
-$(window).on("scroll", function() {
-  var winT = jQuery(window).scrollTop(),
-    winH = jQuery(window).height(),
-    skillsT = skillsDiv.offset().top;
-  if (winT + winH > skillsT) {
-    $(".html").animate({ width: "90%" }, 2000);
-    $(".css").animate({ width: "90%" }, 2000);
-    $(".js").animate({ width: "80%" }, 2000);
-    $(".jQuery").animate({ width: "60%" }, 2000);
-    $(".sass").animate({ width: "70%" }, 2000);
-    $(".boot").animate({ width: "70%" }, 2000);
-    $(".git").animate({ width: "75%" }, 2000);
-    $(".ps").animate({ width: "75%" }, 2000);
-  }
+$(window).on('scroll', function() {
+	var winT = jQuery(window).scrollTop(),
+		winH = jQuery(window).height(),
+		skillsT = skillsDiv.offset().top;
+	if (winT + winH > skillsT) {
+		$('.html').animate({ width: '90%' }, 2000);
+		$('.css').animate({ width: '90%' }, 2000);
+		$('.js').animate({ width: '80%' }, 2000);
+		$('.jQuery').animate({ width: '60%' }, 2000);
+		$('.sass').animate({ width: '70%' }, 2000);
+		$('.boot').animate({ width: '70%' }, 2000);
+		$('.git').animate({ width: '75%' }, 2000);
+		$('.react').animate({ width: '70%' }, 2000);
+	}
 });
